9964fd99233be0029340b1d055c065a2eab0740c
Hasan Mahmud <hasan.mahmud8177@gmail.com>
2021-07-05 22:52:30 +0600

    Initial Setup Completed

---
+++ src/main/java/com/travelagency/travelagency/TravelAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.travelagency.travelagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelagency/travelagency/TravelAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelagency.travelagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
